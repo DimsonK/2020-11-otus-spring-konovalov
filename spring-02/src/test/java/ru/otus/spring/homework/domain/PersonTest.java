3d8323182d8478b45278206f7049d4241b5bf006
Konovalov Dmitriy <dimsonk@gmail.com>
2020-12-03 14:46:13 +0300

    Homework spring-02 done

---
+++ spring-02/src/test/java/ru/otus/spring/homework/domain/PersonTest.java
@@ -0,0 +1,26 @@
+package ru.otus.spring.homework.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Person class test")
+class PersonTest {
+
+    @DisplayName("class create is correct")
+    @Test
+    void correctClassCreate() {
+        Person person = new Person("FirstName", "LastName");
+
+        assertAll("person",
+                () -> assertEquals("FirstName", person.getFirstName()),
+                () -> assertEquals("LastName", person.getLastName()));
+        assertNotNull(person.getTestResultList());
+
+        TestResult result = new TestResult("1", "2", false);
+        person.addTestResult(result);
+
+        assertEquals(1, person.getTestResultList().size());
+    }
+}
